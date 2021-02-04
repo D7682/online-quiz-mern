78b7182c9dc857ad1b74bd783b1dbc791b5c7078
saikat1095 <mdabutahersaikat@gmail.com>
2021-02-04 21:12:18 +0600

    clien login/logout added

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ db.mongoose
   })
   .then(() => {
     console.log("Successfully connect to MongoDB.");
-    // initial();
+    initial();
   })
   .catch(err => {
     console.error("Connection error", err);
@@ -31,7 +31,6 @@ db.mongoose
           if (err) {
             console.log("error", err);
           }
-  
           console.log("added 'user' to roles collection");
         });
   
@@ -41,7 +40,6 @@ db.mongoose
           if (err) {
             console.log("error", err);
           }
-  
           console.log("added 'moderator' to roles collection");
         });
   
